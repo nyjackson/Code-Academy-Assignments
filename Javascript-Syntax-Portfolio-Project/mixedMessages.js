2f5d895cd9a9d5dcaa659fe12bd197b03df68f94
njackson <nyalajackson@yahoo.com>
2021-09-23 20:13:10 -0400

    Creation of mixedMessages project

---
+++ Javascript-Syntax-Portfolio-Project/mixedMessages.js
@@ -0,0 +1,7 @@
+const messages = {
+beginning: ["Don't underestimate yourself!", "You are capable of greatness."],
+middle:["But don't overestimate yourself either.", "Only if you try!"],
+end:["Be yourself.", "Be someone else."]
+};
+
+console.log("Hello")
