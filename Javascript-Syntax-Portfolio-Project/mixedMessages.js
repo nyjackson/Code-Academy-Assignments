3f9482c84c2e1872574899cdd3750e46dd19e81e
njackson <nyalajackson@yahoo.com>
2021-09-23 20:45:17 -0400

    Made selectRandom and getRandSentence separate to allow for more conditions

--- Javascript-Syntax-Portfolio-Project/mixedMessages.js
+++ Javascript-Syntax-Portfolio-Project/mixedMessages.js
@@ -1,13 +1,13 @@
 const trafficConditions = {
     density: ['Light', 'Heavy', 'Medium','Rush Hour'],
     weather:['Rainy','Sunny', 'Snowy', 'Cloudy'],
-    delay: ['no', 'an hour', 'a 2 hour', ' a 30 min'],
-    selectRandom(){
-        let densityRand = this.density[Math.floor(Math.random() * this.density.length)]
-        let weatherRand = this.weather[Math.floor(Math.random() * this.weather.length)]
-        let delayRand = this.delay[Math.floor(Math.random() * this.delay.length)]
-        return `Traffic is currently ${densityRand}, with ${delayRand} delay. The weather is ${weatherRand}`
+    delay: ['no', 'an hour', 'a 2 hour', 'a 30 min'],
+    selectRandom(arr){
+        return arr[Math.floor(Math.random() * arr.length)]
     },
+    getRandSentence(){
+        return `Traffic is currently ${this.selectRandom(this.density)}, with ${this.selectRandom(this.delay)} delay. \nThe weather is ${this.selectRandom(this.weather)}.`
+    }
 };
 
-console.log(trafficConditions.selectRandom())
+console.log(trafficConditions.getRandSentence())
